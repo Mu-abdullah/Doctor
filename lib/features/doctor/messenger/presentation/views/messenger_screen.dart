c1835c7b2b0cc3233f72260a8752dcdcbf045522
Mu-abdullah <moabdullah588@gmail.com>
2025-06-04 19:39:17 +0300

    feat: Refactor MessengerScreen layout and improve code structure

--- lib/features/doctor/messenger/presentation/views/messenger_screen.dart
+++ lib/features/doctor/messenger/presentation/views/messenger_screen.dart
@@ -1,5 +1,8 @@
-import 'package:doctor/core/style/widgets/app_text.dart';
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
+
+import '../../../../../core/style/color/app_color.dart';
+import '../../../../../core/style/widgets/app_text.dart';
 
 class MessengerScreen extends StatelessWidget {
   const MessengerScreen({super.key});
@@ -8,15 +11,33 @@ class MessengerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: 50,
-      itemBuilder:
-          (context, index) => Padding(
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: AppColors.blueAccent.withValues(alpha: 0.1),
+              borderRadius: BorderRadius.circular(12),
+            ),
             padding: const EdgeInsets.all(8.0),
-            child: Container(
-              color: Colors.blue,
-              padding: const EdgeInsets.all(8.0),
-              child: AppText("text", translate: false),
+            child: Row(
+              spacing: 16,
+              children: [
+                Container(
+                  width: 48,
+                  height: 48,
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent.withValues(alpha: 0.2),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(HugeIcons.strokeRoundedPatient),
+                ),
+                const AppText("Patient name", translate: false),
+              ],
             ),
           ),
+        );
+      },
     );
   }
 }
