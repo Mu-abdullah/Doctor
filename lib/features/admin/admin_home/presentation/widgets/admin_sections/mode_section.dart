e42aba66a730c3c9dc39260610e79b210fb4a08b
Mu-abdullah <moabdullah588@gmail.com>
2025-06-08 16:49:02 +0300

    feat: Refactor ModeSection to use GridView for better layout and add TestScreen implementation

--- lib/features/admin/admin_home/presentation/widgets/admin_sections/mode_section.dart
+++ lib/features/admin/admin_home/presentation/widgets/admin_sections/mode_section.dart
@@ -14,58 +14,69 @@ class ModeSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var items = getModeSectionItems(context);
     return AdminSections(
       title: LangKeys.modes,
       icon: HugeIcons.strokeRoundedChangeScreenMode,
-      child: Column(
-        spacing: 16,
-        children: [
-          AdminHomeCustomButton(
-            title: LangKeys.doctor,
-            icon: HugeIcons.strokeRoundedDoctor01,
-            onTap:
-                () => context.pushNamed(
-                  RoutesNames.doctorHome,
-                  arguments: {'isAdmin': true},
-                ),
-          ),
-          AdminHomeCustomButton(
-            title: LangKeys.student,
-            icon: HugeIcons.strokeRoundedStudent,
-            color: AppColors.green,
-            onTap:
-                () => context.pushNamed(
-                  RoutesNames.studentHomeScreen,
-                  arguments: {'isAdmin': true},
-                ),
-          ),
-          AdminHomeCustomButton(
-            title: LangKeys.patient,
-            icon: HugeIcons.strokeRoundedPatient,
-            color: AppColors.yellow,
-            onTap: () {
-              CustomSnackbar.showTopSnackBar(
-                context,
-                message: LangKeys.comingSoon,
-                backgroundColor: AppColors.yellow,
-                top: false,
-              );
-            },
-          ),
-          AdminHomeCustomButton(
-            title: LangKeys.assistant,
-            icon: HugeIcons.strokeRoundedDoctor02,
-            color: AppColors.black,
-            textColor: AppColors.white,
-            onTap: () {
-              context.pushNamed(
-                RoutesNames.assistantScreen,
-                arguments: {'isAdmin': true},
-              );
-            },
-          ),
-        ],
+      child: GridView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          childAspectRatio: 16 / 9,
+        ),
+        itemBuilder: (context, index) => items[index],
+        itemCount: items.length,
       ),
     );
   }
 }
+
+List<Widget> getModeSectionItems(BuildContext context) => [
+  AdminHomeCustomButton(
+    title: LangKeys.doctor,
+    icon: HugeIcons.strokeRoundedDoctor01,
+    onTap:
+        () => context.pushNamed(
+          RoutesNames.doctorHome,
+          arguments: {'isAdmin': true},
+        ),
+  ),
+  AdminHomeCustomButton(
+    title: LangKeys.student,
+    icon: HugeIcons.strokeRoundedStudent,
+    color: AppColors.green,
+    onTap:
+        () => context.pushNamed(
+          RoutesNames.studentHomeScreen,
+          arguments: {'isAdmin': true},
+        ),
+  ),
+  AdminHomeCustomButton(
+    title: LangKeys.patient,
+    icon: HugeIcons.strokeRoundedPatient,
+    color: AppColors.yellow,
+    onTap: () {
+      CustomSnackbar.showTopSnackBar(
+        context,
+        message: LangKeys.comingSoon,
+        backgroundColor: AppColors.yellow,
+        top: false,
+      );
+    },
+  ),
+  AdminHomeCustomButton(
+    title: LangKeys.assistant,
+    icon: HugeIcons.strokeRoundedDoctor02,
+    color: AppColors.black,
+    textColor: AppColors.white,
+    onTap: () {
+      context.pushNamed(
+        RoutesNames.assistantScreen,
+        arguments: {'isAdmin': true},
+      );
+    },
+  ),
+];
